425b2124ce7a629fa880bcf4b6f922288ddcfa94
barreirojm <barreirojuanmanuel@gmail.com>
2022-07-25 20:13:03 -0300

    spring ini

---
+++ src/test/java/com/porfolio/barreirojm/BarreirojmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolio.barreirojm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarreirojmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
